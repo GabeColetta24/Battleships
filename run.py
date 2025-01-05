c063789be3fb4faa823fa5df0ffac1a8548af7df
Gabe Coletta <houfmtub@students.codeinstitute.net>
2025-01-05 16:08:31 +0000

    Add user input function for shooting

--- run.py
+++ run.py
@@ -19,3 +19,13 @@ def place_ships(grid, ship_count):
             if grid[x][y] == "~":
                 grid[x][y] = "S"
                 break
+
+def get_user_input():
+    """Gets the user's shot input."""
+    while True:
+        try:
+            coords = input("Enter your shot (row,col): ")
+            x, y = map(int, coords.split(","))
+            return x, y
+        except ValueError:
+            print("Invalid input. Please enter in the format row,col (e.g., 1,2).")
